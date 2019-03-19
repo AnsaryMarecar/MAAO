57aa6dca68b1eb6fc165602bf02658be1da6209d
AnsaryMarecar <an.marecar@gmail.com>
2019-03-19 17:10:44 +0100

    oussama.bouachrine
    socketServer and socketClient ready

--- client/socketclient/ClientAdapteur.java
+++ client/socketclient/ClientAdapteur.java
@@ -1,10 +1,14 @@
 package socketclient;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
 import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 
 public class ClientAdapteur {
@@ -12,33 +16,30 @@ public class ClientAdapteur {
 
 
 	public static void main(String[] args) {
-		
-			InetAddress LocaleAdresse ;
-			BufferedReader in;
-	try {
 		Socket socket;
-		
-			LocaleAdresse = InetAddress.getLocalHost();
-			System.out.println("L'adresse locale est : "+LocaleAdresse );
-			
-			   socket = new Socket( LocaleAdresse,5009);	
-			   System.out.println("Demande de connexion");
-			   in = new BufferedReader ( new InputStreamReader (socket.getInputStream()));
-			   String message_dist = in.readLine();
-			   System.out.println(message_dist);
-			   
-	           socket.close();
+		BufferedReader in;
+		PrintWriter out;
 
+		try {
+		
+			socket = new Socket(InetAddress.getLocalHost(),2009);	
+			JOptionPane.showMessageDialog(null,"Demande de connexion",  "client", JOptionPane.ERROR_MESSAGE);
+		        System.out.println("Demande de connexion");
+
+		        in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+		        String message_distant = in.readLine();
+		    	JOptionPane.showMessageDialog(null,message_distant,  "client", JOptionPane.ERROR_MESSAGE);
+		        System.out.println(message_distant);
+		        
+		        socket.close();
+		       
+		}catch (UnknownHostException e) {
 			
-		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
 			
 			e.printStackTrace();
 		}
-	catch (IOException e) {
-		
-		e.printStackTrace();
-	}
-
 	}
 	}
 
--- service/socketserver/ServerAdaptor.java
+++ service/socketserver/ServerAdaptor.java
@@ -1,36 +1,42 @@
 package socketserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.swing.JOptionPane;
+
 public class ServerAdaptor {
 	
 	
-	public static void main(String[] args) throws IOException {
-		ServerSocket socketServer;
-		Socket socketofServer;
-		InetAddress localAdress;
+	public static void main(String[] args)  {
+		ServerSocket socketserver  ;
+		Socket socketduserveur ;
+		BufferedReader in;
 		PrintWriter out;
+		
 		try {
-			localAdress= InetAddress.getLocalHost();
-			socketServer = new ServerSocket(5009, 1000,localAdress );
-			socketofServer= socketServer.accept();
-			System.out.println("Quelqu'un est connecté");
-			out = new PrintWriter(socketofServer.getOutputStream());
-	        out.println("Vous êtes connecté zéro !");
-	        out.flush();
+		
+			socketserver = new ServerSocket(2009);
+			JOptionPane.showMessageDialog(null, "Le serveur est à l'écoute du port "+socketserver.getLocalPort(),  "serveur", JOptionPane.ERROR_MESSAGE);
+			System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());
+			socketduserveur = socketserver.accept(); 
+			JOptionPane.showMessageDialog(null, "Un zéro s'est connecté",  "serveur", JOptionPane.ERROR_MESSAGE);
+		        System.out.println("Un zéro s'est connecté");
+			out = new PrintWriter(socketduserveur.getOutputStream());
+			JOptionPane.showMessageDialog(null, "Vous êtes connecté zéro !",  "serveur", JOptionPane.ERROR_MESSAGE);
+		        out.println("Vous êtes connecté zéro !");
+		        out.flush();
+		                
+		        socketduserveur.close();
+		        socketserver.close();
+		        
+		}catch (IOException e) {
 			
-			socketServer.close();
-			socketofServer.close();
-			
-		}
-		catch(Exception e){
 			e.printStackTrace();
 		}
-		
-
 	}
 }
