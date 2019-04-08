c2d7bffe6eacc00fc8fe17f15de2fa876df0e384
AnsaryMarecar <an.marecar@gmail.com>
2019-04-08 15:30:41 +0200

    MainClient modification

---
+++ secure-retirement-home-app/secure-retirement-home-client/src/main/java/org/secure/retirement/home/client/AppClient.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.secure.retirement.home.client;
+
+import org.secure.retirement.home.frame.FrameType_sensor;
+
+/**
+ * @author Ansary MARECAR
+ *
+ */
+public class AppClient {
+	public static void Main(String args) {
+		FrameType_sensor frame_typesensor = new FrameType_sensor();
+	}
+}
