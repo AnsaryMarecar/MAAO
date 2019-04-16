dfddfbb34c1013cc0195f5a1885a7d0e0df46501
ansary.marecar <ansary.marecar@etu.u-pec.fr>
2019-04-16 18:50:59 +0200

    Update Type_sensor

--- secure-retirement-home-app/secure-retirement-home-common/src/main/java/org/secure/retirement/home/common/Type_sensor.java
+++ secure-retirement-home-app/secure-retirement-home-common/src/main/java/org/secure/retirement/home/common/Type_sensor.java
@@ -6,6 +6,7 @@ public class Type_sensor {
 	
 	private int 	type_sensor_id	;
 	private String	type_sensor_name;
+	private int		type_sensor_interval;
 	
 	/**
 	 * 
@@ -14,6 +15,18 @@ public class Type_sensor {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * @param type_sensor_id
+	 * @param type_sensor_name
+	 * @param type_sensor_interval
+	 */
+	public Type_sensor(int type_sensor_id, String type_sensor_name, int type_sensor_interval) {
+		super();
+		this.type_sensor_id 		= type_sensor_id;
+		this.type_sensor_name 		= type_sensor_name;
+		this.type_sensor_interval 	= type_sensor_interval;
+	}
+
 	/**
 	 * @param type_sensor_id
 	 * @param type_sensor_name
@@ -62,6 +75,20 @@ public class Type_sensor {
 	public String toString() {
 		return "Type_sensor [getType_sensor_id()=" + getType_sensor_id() + ", getType_sensor_name()="
 				+ getType_sensor_name() + "]";
+	}
+
+	/**
+	 * @return the type_sensor_interval
+	 */
+	public int getType_sensor_interval() {
+		return type_sensor_interval;
+	}
+
+	/**
+	 * @param type_sensor_interval the type_sensor_interval to set
+	 */
+	public void setType_sensor_interval(int type_sensor_interval) {
+		this.type_sensor_interval = type_sensor_interval;
 	}
 	
 	
