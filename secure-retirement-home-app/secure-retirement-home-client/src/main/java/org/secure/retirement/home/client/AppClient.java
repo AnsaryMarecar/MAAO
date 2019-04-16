3ba9e4c8d7a22b7656452888034c7926c7e99a46
melissio <47556980+melissio@users.noreply.github.com>
2019-04-16 16:36:25 +0200

    frame settings

--- secure-retirement-home-app/secure-retirement-home-client/src/main/java/org/secure/retirement/home/client/AppClient.java
+++ secure-retirement-home-app/secure-retirement-home-client/src/main/java/org/secure/retirement/home/client/AppClient.java
@@ -4,8 +4,8 @@
 package org.secure.retirement.home.client;
 
 import java.util.Locale;
-
 import org.secure.retirement.home.frame.FrameHome;
+import org.secure.retirement.home.frame.FrameSettings;
 import org.secure.retirement.home.frame.FrameType_sensor;
 
 /**
@@ -15,7 +15,8 @@ import org.secure.retirement.home.frame.FrameType_sensor;
 public class AppClient {
 	public static void main (String[] args) {
 		Locale.setDefault(Locale.ENGLISH);
-		FrameHome frameHome = new FrameHome();
+		//FrameHome frameHome = new FrameHome();
+		FrameSettings frameSettings = new FrameSettings();
 		System.out.println("FRAMEHome was launched");
 	}
 
