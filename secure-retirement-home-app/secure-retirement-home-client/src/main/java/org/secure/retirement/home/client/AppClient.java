c98482d12b14d271878fd40db31695db8f36760a
melissio <47556980+melissio@users.noreply.github.com>
2019-04-16 14:47:37 +0200

    locale.english

--- secure-retirement-home-app/secure-retirement-home-client/src/main/java/org/secure/retirement/home/client/AppClient.java
+++ secure-retirement-home-app/secure-retirement-home-client/src/main/java/org/secure/retirement/home/client/AppClient.java
@@ -3,6 +3,8 @@
  */
 package org.secure.retirement.home.client;
 
+import java.util.Locale;
+
 import org.secure.retirement.home.frame.FrameHome;
 import org.secure.retirement.home.frame.FrameType_sensor;
 
@@ -12,6 +14,7 @@ import org.secure.retirement.home.frame.FrameType_sensor;
  */
 public class AppClient {
 	public static void main (String[] args) {
+		Locale.setDefault(Locale.ENGLISH);
 		FrameHome frameHome = new FrameHome();
 		System.out.println("FRAMEHome was launched");
 	}
