09fc7a8152b06934c4905b4b668d110f22dd4436
amine775 <amine_77500@hotmail.fr>
2019-04-16 16:28:50 +0200

    GuiAnalysis

--- secure-retirement-home-app/secure-retirement-home-client/src/main/java/org/secure/retirement/home/frame/FrameAnalysis.java
+++ secure-retirement-home-app/secure-retirement-home-client/src/main/java/org/secure/retirement/home/frame/FrameAnalysis.java
@@ -2,10 +2,14 @@ package org.secure.retirement.home.frame;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.Icon;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -15,56 +19,57 @@ import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
+
 public class FrameAnalysis extends JFrame {
 	
-	private JRadioButton sensor, risq, historic;
+	private JCheckBox sensor, room, historic;
 	private JPanel pan1, pan2, pan3, pan4;
 	private JLabel jl1;
 	private JLayeredPane jlp, jlp2;
-	private JCheckBox sensorList;
+	private JComboBox sensorList, roomList, historicList;
 	
 	
 	
 	public FrameAnalysis() {
 		
 		jl1= new JLabel();
-		sensorList= new JCheckBox();
+		sensorList=new JComboBox();
+		roomList= new JComboBox();
+		historicList= new JComboBox();
 		
 		this.setLocationRelativeTo(null);
-		this.setSize(600, 600);
+		this.setSize(1000, 1000);
 		this.setResizable(false);
 		
-		sensor = new JRadioButton("Sensors");
+		sensor = new JCheckBox("Sensors");
 		sensor.setForeground(Color.WHITE);
 		sensor.setBackground(Color.DARK_GRAY);
+		sensor.addItemListener(new ItemListen());
 		
 		
-		risq = new JRadioButton("Risq");
-		risq.setForeground(Color.WHITE);
-		risq.setBackground(Color.DARK_GRAY);
-		sensor.setActionCommand("Risq");
+		room = new JCheckBox("Room");
+		room.setForeground(Color.WHITE);
+		room.setBackground(Color.DARK_GRAY);
+		room.addItemListener(new ItemListen());
 		
-		historic = new JRadioButton("Historic");
+		historic = new JCheckBox("Historic");
 		historic.setForeground(Color.WHITE);
 		historic.setBackground(Color.DARK_GRAY);
-		sensor.setActionCommand("Historic");
+		historic.addItemListener(new ItemListen());
+		
 		
 		
 		pan1= new JPanel();
 		pan1.setBackground(Color.DARK_GRAY);
-		pan2= new JPanel();
-		pan2.setBackground(Color.LIGHT_GRAY);
+		
 		pan3= new JPanel();
 		pan3.setBackground(Color.DARK_GRAY);
+		
 		pan4= new JPanel();
 		pan4.setBackground(Color.LIGHT_GRAY);
 		
-
-		
-		
 		
 		jlp= new JLayeredPane();
-		jlp2= new JLayeredPane();
 		
 		jlp.add(pan1);
 		jlp.add(pan3);
@@ -73,49 +78,83 @@ public class FrameAnalysis extends JFrame {
 		this.setLayout(new BorderLayout()); 
 		this.getContentPane().add(jlp); 
 		
-		pan1.setBounds(0,0,600, 100);
+		pan1.setBounds(0,0,1000, 200);
 		pan1.add(sensor);
-		pan1.add(risq);
+		pan1.add(room);
 		pan1.add(historic);
 		
-		pan2.setBounds(0,100, 600, 500);
 		
-		pan3.setBounds(0,100,200,500);
-		pan4.setBounds(200,100,400,500);
+		pan3.setBounds(0,200,300,800);
+		pan3.setLayout(new FlowLayout());
+		pan3.add(sensorList);
+		sensorList.setVisible(false);
+		pan3.add(historicList);
+		historicList.setVisible(false);
+		pan3.add(roomList);
+		roomList.setVisible(false);
 		
-		
-		
-		
+
+		pan4.setBounds(300,200,700,800);
+
 		
 		
 		
 		this.setVisible(true);
-		
-
 	}
 	
-	
-	public void actionPerformed (ActionEvent e) {
-		String ac=new String();
-		ac=e.getActionCommand();
-		
-		if (sensor.isSelected()) {
-			pan3.add(sensorList);
+
+	private class ItemListen implements ItemListener {
+
+        public void itemStateChanged(ItemEvent arg0) {
+        
+        // Sensor CheckBox Actions
+		if (sensor.isSelected()==true && sensorList.getItemCount()<1) {
+			Object[] elementSensor = new Object[]{"Sensor 1", "Sensor 2", "Sensor 3", "Sensor 4", "Sensor 5"};
+			for (int i=0; i<elementSensor.length; i++) {
+				String sens=elementSensor[i].toString();
+				sensorList.addItem(sens);
+			}
+
+			System.out.println("salut je vais bien ");
+			sensorList.setVisible(true);
 		}
+		if (sensor.isSelected()==false) {
+			sensorList.removeAllItems();
+			sensorList.setVisible(false);
+		}
+		
+		
+		// Hictoric CheckBox Actions
+		if(historic.isSelected()==true && historicList.getItemCount()<1) {	
+			Object[] elementHistoric = new Object[]{"Historic 1", "Historic 2", "Historic 3", "Historic 4", "Historic 5"};
+			for (int i=0; i<elementHistoric.length; i++) {
+				String hist = elementHistoric[i].toString();
+				historicList.addItem(hist);
+			}
+			historicList.setVisible(true);
+        }
+		if (historic.isSelected()==false) {
+			historicList.removeAllItems();
+			historicList.setVisible(false);
+		}
+		
+		// Room CheckBox Actions
+		if(room.isSelected()==true && roomList.getItemCount()<1) {
+			Object[] elementRoom = new Object[]{"room 1", "room 2", "room 3", "room 4", "room 5"};
+			for (int i=0; i<elementRoom.length; i++) {
+				String room=elementRoom[i].toString();
+				roomList.addItem(room);
+				
+			}
+			roomList.setVisible(true);
+		}
+		if (room.isSelected()==false) {
+			roomList.removeAllItems();
+			roomList.setVisible(false);
+		}
+		
+        }
+
+	
 	}
-
-
-
-	private Object getSensor() {
-		// TODO Auto-generated method stub
-		return sensor;
-	}
-	
-	
-	
-	
-	
-	
-	
-
 }
