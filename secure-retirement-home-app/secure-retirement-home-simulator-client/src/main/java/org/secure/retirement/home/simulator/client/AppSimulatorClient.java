556ccb9e1180f3fc6c2ca5a2b9d839262c28f5af
ansary.marecar <ansary.marecar@etu.u-pec.fr>
2019-05-05 18:06:55 +0200

    Preparation simulator client to send different sensor

--- secure-retirement-home-app/secure-retirement-home-simulator-client/src/main/java/org/secure/retirement/home/simulator/client/AppSimulatorClient.java
+++ secure-retirement-home-app/secure-retirement-home-simulator-client/src/main/java/org/secure/retirement/home/simulator/client/AppSimulatorClient.java
@@ -1,13 +1,12 @@
 package org.secure.retirement.home.simulator.client;
 
 /**
- * Hello world!
  *
  */
 public class AppSimulatorClient 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "" );
     }
 }
