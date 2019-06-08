9dc1f64dca94b214e4f98e5c1a0b27a6f6051e5e
amine775 <amine_77500@hotmail.fr>
2019-06-09 00:01:03 +0200

    Update AnalysisMix.java

--- secure-retirement-home-app/secure-retirement-home-common/src/main/java/org/secure/retirement/home/common/AnalysisMix.java
+++ secure-retirement-home-app/secure-retirement-home-common/src/main/java/org/secure/retirement/home/common/AnalysisMix.java
@@ -6,9 +6,9 @@ public abstract class AnalysisMix {
 	
 	
 	   AnalysisMix(@JsonProperty("sensor_mac") String sensor_name,
-               @JsonProperty("sensor_ip") String sensor_ip,
-               @JsonProperty("type_sensor_name") String type_sensor_name,
-	   			@JsonProperty("room_name") String room_name,
+            @JsonProperty("sensor_ip") String sensor_ip,
+            @JsonProperty("type_sensor_name") String type_sensor_name,
+	   		@JsonProperty("room_name") String room_name,
 			@JsonProperty("historic_datetime") String historic_datetime, 
             @JsonProperty("historic_value") String historic_value){}
 	   
